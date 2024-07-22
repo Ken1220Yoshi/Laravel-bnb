d4ecae3dfccf9a70a6dfa1677fb9b61ee523181e
TOKIMASA1206 <tokihide121062@gmail.com>
2024-07-22 18:26:40 +0900

    added show page changes

---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+
+@section('title', 'Show post')
+
+@section('content')
+
+<div class="Post">
+  <div class=""></div>
+  <h1>Post Title</h1>
+  <div class="Image">
+    imageが入ります。
+  </div>
+</div>
+
+@endsection
