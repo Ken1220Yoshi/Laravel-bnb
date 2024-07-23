529c29d537daf0ba79fc50b820f998a1137a4aa7
吉川建太朗 <kentaro@yoshikawakentarounoMacBook-Air.local>
2024-07-23 14:12:32 +0800

    Post Store

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,3 +18,8 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(["middleware" => "auth"],function(){
+    Route::resource('post',PostController::class);
+});
