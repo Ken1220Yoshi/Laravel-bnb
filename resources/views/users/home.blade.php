20d7bc2f7da5250d4bf7a0068fff846c30bde065
Toast-men <shinta2262@gmail.com>
2024-07-23 16:45:10 +0800

    added home

--- resources/views/users/home.blade.php
+++ resources/views/users/home.blade.php
@@ -5,95 +5,41 @@
 @section('content')
 <div class="container-fluid px-5">
   <div class="row">
-    <div class="col" class="mx-auto">
+
+  @foreach ($all_post as $post )
+    
+  
+    <div class="col-3" class="mx-auto">
       <a href="" class="text-decoration-none text-dark rounded">
         <div class="image-container">
-          <img src="https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-home">
+          <img src="{{$post->image}}" alt="" class="image-home">
+          <i class="fa-solid fa-circle-chevron-right icon-img"></i>
           <form action="" method="post" class="like-form">
             @csrf
             <button type="submit" class="btn btn-none">
               <i class="fa-regular fa-heart icon-md"></i>
             </button>
           </form>
-
-
         </div>
 
 
         <div class="row mt-3">
           <div class="col-9">
-            <p class="h4 fw-bold">places</p>
+            <p class="h4 fw-bold">{{$post->title}}</p>
           </div>
           <div class="col">rate</div>
         </div>
-        <p class="h4">
-          date
-        </p>
 
-        <p class="h4">
-          price
-        </p>
-
-      </a>
-
-    </div>
-    <div class="col" class="mx-auto">
-      <a href="" class="text-decoration-none text-dark">
-        <img src="https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-home">
-        <div class="row">
-          <div class="col-9">
-            <p class="h4 fw-bold">place</p>
-          </div>
-          <div class="col">rate</div>
-        </div>
         <p class="h4">
           date
         </p>
         <p class="h4">
-          price
+          {{$post->price}}
         </p>
-
       </a>
-
     </div>
-    <div class="col" class="mx-auto">
-      <a href="" class="text-decoration-none text-dark">
-        <img src="https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-home">
-        <div class="row">
-          <div class="col-9">
-            <p class="h4 fw-bold">place</p>
-          </div>
-          <div class="col">rate</div>
-        </div>
-        <p class="h4">
-          date
-        </p>
-        <p class="h4">
-          price
-        </p>
 
-      </a>
-
-    </div>
-    <div class="col" class="mx-auto">
-      <a href="" class="text-decoration-none text-dark">
-        <img src="https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-home">
-        <div class="row">
-          <div class="col-9">
-            <p class="h4 fw-bold">place</p>
-          </div>
-          <div class="col">rate</div>
-        </div>
-        <p class="h4">
-          date
-        </p>
-        <p class="h4">
-          price
-        </p>
-
-      </a>
-
-    </div>
+    @endforeach
   </div>
 </div>
 
