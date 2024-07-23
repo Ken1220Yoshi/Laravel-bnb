d55e62175d71da731947016cf8dccf543f23c2a4
Toast-men <shinta2262@gmail.com>
2024-07-23 14:38:10 +0800

    added home

---
+++ resources/views/users/home.blade.php
@@ -0,0 +1,93 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col" class="mx-auto">
+      <a href="" class="text-decoration-none text-dark rounded">
+        <div class="image-container">
+          <img src="https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-home">
+          <i class="fa-regular  fa-heart icon-md"></i>
+        </div>
+
+
+        <div class="row mt-3">
+          <div class="col-9">
+            <p class="h4 fw-bold">place</p>
+          </div>
+          <div class="col">rate</div>
+        </div>
+        <p class="h4">
+          date
+        </p>
+
+        <p class="h4">
+          price
+        </p>
+
+      </a>
+
+    </div>
+    <div class="col" class="mx-auto">
+      <a href="" class="text-decoration-none text-dark">
+        <img src="https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-home">
+        <div class="row">
+          <div class="col-9">
+            <p class="h4 fw-bold">place</p>
+          </div>
+          <div class="col">rate</div>
+        </div>
+        <p class="h4">
+          date
+        </p>
+        <p class="h4">
+          price
+        </p>
+
+      </a>
+
+    </div>
+    <div class="col" class="mx-auto">
+      <a href="" class="text-decoration-none text-dark">
+        <img src="https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-home">
+        <div class="row">
+          <div class="col-9">
+            <p class="h4 fw-bold">place</p>
+          </div>
+          <div class="col">rate</div>
+        </div>
+        <p class="h4">
+          date
+        </p>
+        <p class="h4">
+          price
+        </p>
+
+      </a>
+
+    </div>
+    <div class="col" class="mx-auto">
+      <a href="" class="text-decoration-none text-dark">
+        <img src="https://images.pexels.com/photos/259588/pexels-photo-259588.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" class="image-home">
+        <div class="row">
+          <div class="col-9">
+            <p class="h4 fw-bold">place</p>
+          </div>
+          <div class="col">rate</div>
+        </div>
+        <p class="h4">
+          date
+        </p>
+        <p class="h4">
+          price
+        </p>
+
+      </a>
+
+    </div>
+  </div>
+</div>
+
+@endsection
