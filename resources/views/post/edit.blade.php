392a3930cbf405e7d73630a6e4da2ec6092923fd
kurt john <kredoteacher.kurt@gmail.com>
2024-07-24 16:28:45 +0800

    added fixes

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('title','Edit Post')
-    
+
 @section('content')
     <div class="container">
         <div class="header text-center w-75 mx-auto">
@@ -23,13 +23,13 @@
                         <div class="col">
                             <label for="aminities" class="form-label fw-bold">Amenities</label><br>
                             @forelse ($all_amenities as $amenity)
-                                <input type="checkbox" class="form-check-inline form-check-input me-0"   name="amenity[]" id="{{$amenity->id}}" value="{{ $amenity->id }}" 
+                                <input type="checkbox" class="form-check-inline form-check-input me-0"   name="amenity[]" id="{{$amenity->id}}" value="{{ $amenity->id }}"
                                 @forelse ($post->amenityPost as $amenity_post)
                                     @if ($amenity->id === $amenity_post->amenity_id)
                                     checked
-                                    @else
+
                                     @endif
-                                @empty 
+                                @empty
                                 @endforelse
                                 >
                                 <label for="{{$amenity->name}}" class="form-check-label me-2">{{$amenity->name}}</label>
@@ -38,7 +38,7 @@
                         </div>
                     </div>
                 </div>
-    
+
                 <div class="row mb-3">
                     <div class="col">
                         <label for="price" class="form-label fw-bold">Price</label>
@@ -48,9 +48,9 @@
                         </div>
                     </div>
                 </div>
-    
-    
-    
+
+
+
                 <div class="row mb-3">
                     <div class="col">
                         <label for="description" class="form-label fw-bold">Description</label>
@@ -58,14 +58,14 @@
                             placeholder="Description">{{$post->description}}</textarea>
                     </div>
                 </div>
-    
-    
+
+
                 <div class="row mb-3">
                     <div class="col">
                         <label for="images" class="form-label fw-bold">Image</label>
                         <div class="row mb-3">
                             @forelse ($post->images as $image)
-                            
+
                                 <div class="col-4">
                                     <img src="{{$image->image}}" alt="" width="100%" height="200px">
                                 </div>
@@ -77,8 +77,8 @@
                         Max file is 1000kb
                     </div>
                 </div>
-    
-    
+
+
                 <input type="submit" name="submit" value="Post" class="btn btn-primary w-25">
             </form>
         </div>
