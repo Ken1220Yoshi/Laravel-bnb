19eb8766a23d0cb611388a3ebb248137afda0900
TOKIMASA1206 <tokihide121062@gmail.com>
2024-07-25 15:26:15 +0900

    show

--- routes/web.php
+++ routes/web.php
@@ -16,14 +16,11 @@ Route::group(["middleware" => "auth"],function(){
     Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
     Route::resource('post',PostController::class);
-<<<<<<< Updated upstream
     Route::get('/post/delete/{post}',[PostController::class,'delete'])->name('post.delete');
 
     Route::resource('reservation', ReservationController::class);
-=======
 
     Route::resource('user',UserController::class);
->>>>>>> Stashed changes
 });
 
 
